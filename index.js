38ce4bc8c88c1c5e05470ad945607cdc3523e9e1
Valtteri Andström <valtteri.andstrom@gmail.com>
2023-03-16 15:53:06 +0200

    testing auto deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Helsinki</h1>')
+  res.send('<h1>Hello Helsinki!</h1>')
 })
 
 const PORT = 8080
